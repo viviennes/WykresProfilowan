a3c4739b07f5dad9d7c3846ee5125278c2c508f6
Viviennes <gwiazdka6666@gmail.com>
2018-04-07 19:06:03 +0200

    Initial commit for project. basic view

---
+++ WykresProfilowan/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WykresProfilowan
+{
+    using System;
+
+    using OxyPlot;
+    using OxyPlot.Series;
+
+    class MainViewModel
+    {
+        public MainViewModel()
+        {
+            this.MyModel = new PlotModel { Title = "Example 1" };
+            this.MyModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+
+        }
+
+        public PlotModel MyModel { get; private set; }
+    }
+}
--- WykresProfilowan/MainWindow.xaml.cs
+++ WykresProfilowan/MainWindow.xaml.cs
@@ -22,6 +22,7 @@ namespace WykresProfilowan
     {
         public MainWindow()
         {
+
             InitializeComponent();
         }
     }
